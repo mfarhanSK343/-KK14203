5db970e77dc170276e8efcfe01985d930004ae0e
mfarhanSK343 <68146967+mfarhanSK343@users.noreply.github.com>
2020-07-23 15:28:09 +0800

    Add files via upload

---
+++ Lab 5/TestCake.java
@@ -0,0 +1,12 @@
+public class TestCake{
+    public static void main(String args[]){
+       Cake c = new BlackForest("BlackForest");
+       String[] topping = {"Chocolate", "Cherries", "Whipped Cream"};
+       c.setCake(topping, 45.00, 65.00, 80.00);
+       c.printCake();
+      
+       String[] order = {"Chocolate", "Cherries"};
+       c.orderCake(order, 1, 2);
+       c.printOrder(c.getSize(), c.getTotalPrice(c.getSizePrice()));
+    }
+}
